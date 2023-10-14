fbc6b38b45c6211020161a69fd2fafc1bf76efb3
Dibbo T <dibbotripura7@gmail.com>
2023-10-14 19:31:47 +0600

    index.js has been created and updated

---
+++ index.js
@@ -0,0 +1,6 @@
+const apiAccessKey = "tT2f_3q9aEaNzzHqKNkrjeEyCQFrcOFTDhjP1VFfE2s";
+
+
+const formElement = document.querySelector("form");
+const inputElement = document.getElementById("search-image-input")
+const searchDisplay = document.querySelector(".search-images-container")
